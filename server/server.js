9ea0d6e84537b0ad8bc1219798071bfc2adc995c
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-10-06 17:46:08 +0530

    project structure changes made

--- server/server.js
+++ server/server.js
@@ -1,21 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import http from 'http'
-import { Server } from 'socket.io'
-
-//express settings
-const app = express()
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-//socket-io settings
-const server = http.createServer(app)
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-})
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.get('/', function (req, res) {
   res.send('Hello from the server!')
@@ -35,6 +20,6 @@ io.on('connection', function (socket) {
 //we have hard coded the port number here just for convenience
 const PORT = 5000
 
-server.listen(PORT, function () {
+http.listen(PORT, function () {
   console.log(`listening on port : ${PORT}`)
 })
